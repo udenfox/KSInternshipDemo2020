a0195694c09b0f3781a8b34444768b6ea6a64571
d.shevtsov <d.shevtsov@keepsolid.com>
2020-07-07 11:45:55 +0300

    Step1: initial preparation

--- app/src/main/java/com/keepsolid/ksinternshipdemo2020/activity/SecondActivity.java
+++ app/src/main/java/com/keepsolid/ksinternshipdemo2020/activity/SecondActivity.java
@@ -6,13 +6,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.keepsolid.ksinternshipdemo2020.R;
 
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-    }
+        setContentView(R.layout.activity_second);
 
+    }
 }
--- app/src/main/java/com/keepsolid/ksinternshipdemo2020/activity/ThirdActivity.java
+++ app/src/main/java/com/keepsolid/ksinternshipdemo2020/activity/ThirdActivity.java
@@ -1,18 +1,14 @@
 package com.keepsolid.ksinternshipdemo2020.activity;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.keepsolid.ksinternshipdemo2020.R;
+import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ThirdActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
+        setContentView(R.layout.activity_third);
     }
-
 }
