cc5dde00f1b2fbe994e81f4dac089934f28b8a98
d.shevtsov <d.shevtsov@keepsolid.com>
2020-07-07 12:26:27 +0300

    Step5: Open random stuff with intents. Methods extracted.

--- app/src/main/java/com/keepsolid/ksinternshipdemo2020/activity/MainActivity.java
+++ app/src/main/java/com/keepsolid/ksinternshipdemo2020/activity/MainActivity.java
@@ -1,7 +1,9 @@
 package com.keepsolid.ksinternshipdemo2020.activity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,15 +15,21 @@ public class MainActivity extends AppCompatActivity {
 
     private AppCompatButton explicitBtn;
     private AppCompatButton implicitBtn;
+    private AppCompatButton emailBtn;
+    private AppCompatButton browserBtn;
+    private AppCompatButton settingsBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        explicitBtn = findViewById(R.id.btn_open_explicit);
-        implicitBtn = findViewById(R.id.btn_open_implicit);
+        initViews();
+        setListeners();
 
+    }
+
+    private void setListeners() {
         explicitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -36,6 +44,34 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        emailBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendEmail("somemail@mail.com", "Test message!", "Hello world!");
+            }
+        });
+
+        browserBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openUrl("https://www.google.com");
+            }
+        });
+
+        settingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSettings();
+            }
+        });
+    }
+
+    private void initViews() {
+        explicitBtn = findViewById(R.id.btn_open_explicit);
+        implicitBtn = findViewById(R.id.btn_open_implicit);
+        emailBtn = findViewById(R.id.btn_send_email);
+        browserBtn = findViewById(R.id.btn_open_google);
+        settingsBtn = findViewById(R.id.btn_open_settings);
     }
 
     public void openSecondActivityExplicit() {
@@ -46,6 +82,27 @@ public class MainActivity extends AppCompatActivity {
     public void openThirdActivityImplicit() {
         Intent explicitIntent = new Intent("com.keepsolid.ksinternshipdemo2020.ACTION_THIRD_ACTIVITY");
         startActivity(explicitIntent);
+    }
+
+    public void openUrl(String url) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        startActivity(browserIntent);
+    }
+
+    public void sendEmail(String email, String subject, String message) {
+
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_EMAIL, email);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+
+        intent.setType("message/rfc822");
+        startActivity(intent);
+    }
+
+    public void openSettings() {
+        Intent settingsIntent = new Intent(Settings.ACTION_SETTINGS);
+        startActivity(settingsIntent);
     }
 
 }
