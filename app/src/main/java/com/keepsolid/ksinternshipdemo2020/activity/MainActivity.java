475834affd6a2b1252cc63792b028da72016facc
d.shevtsov <d.shevtsov@keepsolid.com>
2020-07-07 12:13:37 +0300

    Step4: Two activities with same action; onClickListener in code.

--- app/src/main/java/com/keepsolid/ksinternshipdemo2020/activity/MainActivity.java
+++ app/src/main/java/com/keepsolid/ksinternshipdemo2020/activity/MainActivity.java
@@ -5,24 +5,45 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import com.keepsolid.ksinternshipdemo2020.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AppCompatButton explicitBtn;
+    private AppCompatButton implicitBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        explicitBtn = findViewById(R.id.btn_open_explicit);
+        implicitBtn = findViewById(R.id.btn_open_implicit);
+
+        explicitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSecondActivityExplicit();
+            }
+        });
+
+        implicitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openThirdActivityImplicit();
+            }
+        });
+
     }
 
-    public void openSecondActivityExplicit(View v) {
+    public void openSecondActivityExplicit() {
         Intent explicitIntent = new Intent(MainActivity.this, SecondActivity.class);
         startActivity(explicitIntent);
     }
 
-    public void openThirdActivityImplicit(View v) {
+    public void openThirdActivityImplicit() {
         Intent explicitIntent = new Intent("com.keepsolid.ksinternshipdemo2020.ACTION_THIRD_ACTIVITY");
         startActivity(explicitIntent);
     }
